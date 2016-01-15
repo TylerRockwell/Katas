19915238875b80b56ce597361195ec8704f9e0c6
Tyler Rockwell <tyler.rockwell87@gmail.com>
2016-01-15 17:24:43 -0500

    Add criteria for 400. Complete Roman Numeral Kata for real.

--- roman_numerals/roman_numerals.rb
+++ roman_numerals/roman_numerals.rb
@@ -3,8 +3,8 @@ class Numerals
     input
     numerals = ""
 
-    roman_numerals = {1000 => "M", 900 => "CM", 500 => "D", 100 => "C",
-      50 => "L", 10 => "X", 9 => "IX", 5=> "V", 4 => "IV", 1 => "I"}
+    roman_numerals = {1000 => "M", 900 => "CM", 500 => "D", 400 => "CD",
+       100 => "C", 50 => "L", 10 => "X", 9 => "IX", 5=> "V", 4 => "IV", 1 => "I"}
 
     roman_numerals.each do |number, numeral|
       if input >= number
@@ -12,7 +12,7 @@ class Numerals
         input %= number
       end
     end
-    
+
     numerals
   end
 end
--- roman_numerals/roman_numerals_spec.rb
+++ roman_numerals/roman_numerals_spec.rb
@@ -18,6 +18,10 @@ describe Numerals do
     expect(@numerals.numeralize(500)).to eq("D")
   end
 
+  it "should handle four hundred" do
+    expect(@numerals.numeralize(400)).to eq("CD")
+  end
+
   it "should handle one hundred" do
     expect(@numerals.numeralize(100)).to eq("C")
   end
@@ -43,6 +47,21 @@ describe Numerals do
   end
 
   it "should handle one" do
+    expect(@numerals.numeralize(1)).to eq("I")
+  end
+
+  it "should work with all these numbers" do
+    expect(@numerals.numeralize(1000)).to eq("M")
+    expect(@numerals.numeralize(900)).to eq("CM")
+    expect(@numerals.numeralize(700)).to eq("DCC")
+    expect(@numerals.numeralize(450)).to eq("CDL")
+    expect(@numerals.numeralize(225)).to eq("CCXXV")
+    expect(@numerals.numeralize(75)).to eq("LXXV")
+    expect(@numerals.numeralize(37)).to eq("XXXVII")
+    expect(@numerals.numeralize(18)).to eq("XVIII")
+    expect(@numerals.numeralize(9)).to eq("IX")
+    expect(@numerals.numeralize(4)).to eq("IV")
+    expect(@numerals.numeralize(2)).to eq("II")
     expect(@numerals.numeralize(1)).to eq("I")
   end
 end
